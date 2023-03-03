b38545b888d62630e7d51697b21a7836bd0e7c0c
bruceabrigo <bruceabrigo@outlook.com>
2023-03-02 21:56:33 -0600

    Access to admin page, and added superuser. Finch Index, now pulls Finch data from SQL db

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
+from .models import Finch
+
 # create dumby finch data in a python list 
 # # These will not be saved to a database, but will be used for reference to verify functionality
 
-finches = [
-    {'scientific_name': 'European Goldfinch', 'color': 'brown and white', 'description': 'small bird, got some rad colors', 'cluster_size': 2},
-    {'scientific_name': 'Hawkfin', 'color': 'Orange, brown, and white', 'description': 'small bird, looks like an orange creamcicle lowkey', 'cluster_size': 4},
-]
+# finches = [
+#     {'scientific_name': 'European Goldfinch', 'color': 'brown and white', 'description': 'small bird, got some rad colors', 'cluster_size': 2},
+#     {'scientific_name': 'Hawkfin', 'color': 'Orange, brown, and white', 'description': 'small bird, looks like an orange creamcicle lowkey', 'cluster_size': 4},
+# ]
 
 # Create your views here.
 
@@ -21,7 +23,7 @@ def about(request):
     return render(request, 'about.html')
     
 def finch_index(request):
-    # take a request, and render a view
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
